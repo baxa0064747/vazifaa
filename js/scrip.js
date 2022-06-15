2152f70c34377c6e89a3cbf449e546ac11ceab53
baxa0064747 <baxrom0064747@gmail.com>
2022-06-16 04:38:00 +0500

    git init .

---
+++ js/scrip.js
@@ -0,0 +1,104 @@
+
+// BIRINCHI MASALA 
+
+// function addition(a, b) {
+// 	return a+b;
+// }
+// console.log(addition(7,2));
+
+
+
+
+
+// IKKINCHI MASALA 
+
+// function convert(minutes) {
+// 	return minutes * 60;
+// }
+// console.log(convert());
+
+
+
+
+
+// UCHINCHI MASALA 
+
+// function addition(num) {
+// 	return num + 1;
+// }
+// console.log(addition(4));
+
+
+
+// TO'RTINCHI MASALA 
+
+
+// function triArea(base, height) {
+// 	return (base*height)/2
+// }
+// console.log(triArea(5, 5));
+
+
+
+
+// BESHINCHI MASALA 
+
+// function calcAge(age) {
+// 	return age*365;
+// }
+
+// console.log(calcAge(1));
+
+
+
+
+
+// OLTINCHI MASALA 
+
+// function remainder(x, y) {
+// 	return x%y;
+// }
+// console.log(remainder(3,3));
+
+
+
+// YETTINCHI MASALA
+
+// function footballPoints(wins, draws, losses) {
+// 	return wins*3+draws*1+losses*0;
+// }
+// console.log(footballPoints(3,2,5));
+
+
+
+// sakizinchi masala
+
+// function points(twoPointers, threePointers) {
+// 	return twoPointers*2+threePointers*3;
+// }
+// console.log(points(7,5));
+
+
+
+
+// to'qizinchi masala
+
+// function lessThan100(a, b) {
+//     if(a+b<100) {
+//         return true
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(lessThan100(50,50));
+
+
+
+
+// o'ninchi masala
+
+// function animals(chickens, cows, pigs) {
+// 	return chickens*2 + cows*4 + pigs*4;
+// }
+// console.log(animals(2,4,6));
+
